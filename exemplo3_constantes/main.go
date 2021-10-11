280ef0723e339b8711d88f692b55878550c8c107
vsrenato <avatarmestre999@gmail.com>
2021-10-11 11:57:28 -0300

    constantes

---
+++ exemplo3_constantes/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	number1, number2 := 7, 3
+
+	fmt.Println(number1 * number2)
+	fmt.Println(number1 + number2)
+	fmt.Println(number1 / number2)
+	fmt.Println(number1 - number2)
+	fmt.Println(number1 * number2)
+}
